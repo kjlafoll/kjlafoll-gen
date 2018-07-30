38784bc4e35b70c2c05c2a9b6304edb18add4fdf
Kyle LaFollette <kylelafollette@Kyle-LaFollettes-MacBook-Pro.local>
2018-07-30 01:32:29 -0700

    First commit

---
+++ Hybrid Sim/hybrid_sim_twostep.py
@@ -0,0 +1,258 @@
+#Author: Kyle J. LaFollette
+#Department of Psychiatry, University of Arizona
+#Correspondance: kjlafoll@psychiatry.arizona.edu
+
+#Runs the simulations of two-stage tasks and analyze them.
+#See simulation parameters to customize model
+
+import os
+import numpy as np
+import pandas as pd
+import random
+from scipy.special import expit
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pickle
+from sklearn.linear_model import LogisticRegression
+
+# Customizable model components
+# ------------------------------------------------------------------------------------------------------
+# Simulation parameters
+ALPHA1, ALPHA2, LMBD, OMEGA, BETA1, BETA2, PERS = 0.5, 0.5, 0.6, 1.0, 5, 5, 0
+# Number of trials
+numtrials = 200
+# Common probability (the rare probability is 1 - commonprob)
+commonprob = 0.7
+# Number of simulated agents of each type (model-free, model-based)
+numagents = 200
+#-------------------------------------------------------------------------------------------------------
+
+paramslist = ((1, ALPHA1, ALPHA2, LMBD, BETA1, BETA2, OMEGA, PERS, None, True),)
+commontrans = {
+    's1a1': 's2s1',
+    's1a2': 's2s2',
+	}
+resultsf = 'sim_results.pkl'
+
+#Gaussian walk; diffuses probability of state 2 reward
+def walk(prob):
+    prob += random.gauss(0, 0.025)
+    if prob < 0.25:
+        prob = 0.50 - prob
+    elif prob > 0.75:
+        prob = 1.50 - prob
+    assert prob >= 0.25 and prob <= 0.75
+    return prob
+
+class Exp:
+    def __init__(self, rwrd_probs=None):
+        if rwrd_probs is None:
+            # Determine initial reward probabilities randomly
+            self.rwrd_probs = {
+                (s, a): random.uniform(0.25, 0.75) \
+                for s in ('s2s1', 's2s2') for a in range(2)}
+        else:
+            self.rwrd_probs = rwrd_probs
+        self.trial = 0
+        self.trial_info = []
+        self.common = None
+        self.finalst = None
+        self.choice = None
+        self.reward = None
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.trial >= numtrials:
+            raise StopIteration
+        self.common = random.random() < commonprob  # Probability of common transition
+        self.trial += 1
+        return self.trial - 1
+    def enter_choice1(self, choice):
+        """Enter the initial-state choice ('s1a1' or 's1a2')."""
+        self.finalst = commontrans[choice]  # Common
+        self.choice = choice
+        if not self.common:  # Rare
+            if self.finalst == 's2s1':
+                self.finalst = 's2s2'
+            else:
+                self.finalst = 's2s1'
+    def enter_choice2(self, choice, diffuse_probs=True):
+        self.reward = random.random() < self.rwrd_probs[(self.finalst, choice)]
+        self.trial_info.append({
+            'trial': self.trial + 1,
+            'common': int(self.common),
+            'choice': self.choice,
+            'finalst': self.finalst,
+            'reward': int(self.reward),
+        })
+        if diffuse_probs:
+            # Diffuse probability
+            for k, v in self.rwrd_probs.items():
+                self.rwrd_probs[k] = walk(v)
+    def get_results(self):
+        """Get dataframe with the results of the experiment."""
+        cols = self.trial_info[0].keys()
+        results = pd.DataFrame(columns=cols)
+        results.trial = results.trial.astype('int')
+        results.common = results.common.astype('int')
+        results.reward = results.reward.astype('int')
+
+        for trial_num, info in enumerate(self.trial_info):
+            results.loc[trial_num] = pd.Series(info)
+        return results
+
+def get_sschoice(q2, beta2, finst):
+    """Get simulated choice.
+    Keyword arguments:
+    q2: dict of final-state action values
+    beta: exploration parameter
+    finst: final state
+    """
+    probs = np.array([np.exp(beta2 * v) for (s, a), v in q2.items() if s == finst])
+    probs /= np.sum(probs)
+    r = random.random()
+    s = 0
+    for action, x in enumerate(probs):
+        s += x
+        if s >= r:
+            return action
+    return action
+
+def hybrid_sim(alpha1, alpha2, lmbd, omega, beta1, beta2, p, rwrd_probs, diffuse_probs):
+    """Simulates a hybrid agent."""
+    q1 = {}
+    q1['s1a1'] = 0
+    q1['s1a2'] = 0
+    q2 = {(s, a): 0 for s in ('s2a1', 's2a2') for a in range(2)}
+    exp = Exp(rwrd_probs)
+    prev_choice = random.choice(('s1a1', 's1a2'))
+    for trial in exp:
+        vpink = max([q2[('s2a1', a)] for a in range(2)])
+        vblue = max([q2[('s2a2', a)] for a in range(2)])
+        # Determine the choice
+        if commontrans['s1a2'] == 's2a2':
+            cv = (2 * commonprob - 1) * (vpink - vblue)
+        else:
+            cv = (2 * commonprob - 1) * (vblue - vpink)
+        rep = 1 if prev_choice == 's1a1' else -1
+        p_left = expit(beta1 * (omega * (cv) + (1 - omega) * (q1['s1a1'] - q1['s1a2']) + rep * p))
+        if random.random() < p_left:
+            a1 = 's1a1'
+        else:
+            a1 = 's1a2'
+        exp.enter_choice1(a1)
+        prev_choice = a1
+        finst = exp.finalst
+        a2 = get_sschoice(q2, beta2, finst)
+        exp.enter_choice2(a2, diffuse_probs=diffuse_probs)
+        r = int(exp.reward)
+        q1[a1] = (1 - alpha1) * q1[a1] + alpha1 * q2[(finst, a2)] + \
+                 alpha1 * lmbd * (r - q2[(finst, a2)])
+        q2[(finst, a2)] = (1 - alpha2) * q2[(finst, a2)] + alpha2 * r
+    return exp
+
+def plot_probs(probs, legend=True):
+    """Plot probabilities."""
+    x = (0, 2)
+    plt.bar(left=x, height=[probs[i] for i in x], align='center',
+            color='tab:orange', label='common')
+    x = (1, 3)
+    plt.bar(left=x, height=[probs[i] for i in x], align='center',
+            color='tab:green', label='rare')
+    plt.xticks((0.5, 2.5), ('rewarded', 'unrewarded'))
+    plt.ylabel('stay probability')
+    if legend:
+        plt.legend(loc='upper right', fontsize='medium')
+    plt.ylim(0, 1)
+    plt.xlim(-0.5, 3.5)
+
+def get_predictors(results):
+    """Get predictors for logistic regression."""
+    assert len(results) == 200
+    y = []
+    x = []
+    for (_, trial1), (_, trial2) in zip(results.iloc[:-1].iterrows(), results.iloc[1:].iterrows()):
+        transition = 2 * int(trial1.common) - 1
+        reward = 2 * trial1.reward - 1
+        x.append([1, reward, transition, reward * transition])
+        y.append(int(trial1.choice == trial2.choice))
+    return x, y
+
+def get_sim_results():
+    """Run simulations and get results, or load results from file."""
+    if not os.path.exists(resultsf):
+        with open(resultsf, 'wb') as outf:
+            for params in paramslist:
+                sim_results = []
+                sim_num, alpha1, alpha2, lmbd, beta1, beta2, omega, pers, rwrd_probs, diffuse_probs = params
+                print('Running simulation {}...'.format(sim_num))
+                for rep in range(numagents):
+                    if rep % 100 == 0:
+                        print('Creating hybrid agents {}-{} of {}...'.format(
+                            rep + 1, rep + 100, numagents))
+                    exp = hybrid_sim(alpha1, alpha2, lmbd, omega, beta1, beta2, pers, rwrd_probs, diffuse_probs)
+                    results = exp.get_results()
+                    sim_results.append(('Hybrid', results))
+
+                pickle.dump((sim_num, sim_results), outf)
+                yield sim_num, sim_results
+    else:
+        with open(resultsf, 'rb') as outf:
+            for params in paramslist:
+                sim_num, sim_results = pickle.load(outf)
+                yield sim_num, sim_results
+
+def run_simulations():
+    """Run simulations, analyze the results, and create figures."""
+    mpl.rcParams['xtick.direction'] = 'out'
+    mpl.rcParams['ytick.direction'] = 'out'
+    analyses = (
+        ('standard', get_predictors),
+    )
+    for sim_num, sim_results in get_sim_results():
+        for ana, get_predictors_f in analyses:
+            figure = plt.figure()
+            figure.set_size_inches(2 * 4, 4)
+            for group_num, group in enumerate(('Hybrid', 'Hybrid')):
+                group_results = [results for g, results in sim_results if g == group]
+                # Perform logistic regression with little regularization
+                # although regulatization doesn't make much difference
+                logreg = LogisticRegression(fit_intercept=False, C=1e6)
+                x, y = [], []
+                for i, results in enumerate(group_results):
+                    xx, yy = get_predictors_f(results)
+                    M = [0] * len(group_results)
+                    M[i] = 1
+                    for l in xx:
+                        x.append(M + l)
+                    y += yy
+                logreg.fit(x, y)
+                del x
+                del y
+
+                axes = plt.subplot()
+                axes.spines['right'].set_color('none')
+                axes.spines['top'].set_color('none')
+                axes.xaxis.set_ticks_position('bottom')
+                axes.yaxis.set_ticks_position('left')
+                plt.title(group)
+                coefs = logreg.coef_[0][
+                        len(group_results):(len(group_results) + 4)]
+                probs = (
+                    expit(coefs[0] + coefs[1] + coefs[2] + coefs[3]),
+                    expit(coefs[0] + coefs[1] - coefs[2] - coefs[3]),
+                    expit(coefs[0] - coefs[1] + coefs[2] - coefs[3]),
+                    expit(coefs[0] - coefs[1] - coefs[2] + coefs[3]),
+                )
+                plot_probs(probs)
+                del group_results
+                del logreg
+
+            plt.tight_layout()
+            plt.savefig(
+                'results-sim{}-{}.pdf'.format(sim_num, ana), bbox_inches='tight')
+            plt.close()
+        del sim_results
+
+if __name__ == '__main__':
+    run_simulations()
---
+++ Peer Influence Exp/PInfMain.py
@@ -0,0 +1,374 @@
+from pynput import keyboard
+from psychopy.visual import *
+from psychopy import monitors
+from psychopy import core
+from random import *
+from time import *
+import os
+import pickle
+
+#===================================================================================================
+# CONSTANTS
+#===================================================================================================
+
+# 8 payoff value vectors
+PAYOFF_BANK = [["$33.2", "$23.1", "$56.8", "$1.7"], ["$20.8", "$15.2", "$37.4", "$1.1"], ["$19.6", "$18.0", "$38.6", "$0.9"], ["$25.5", "$24.9", "$50.8", "$1.3"], ["$24.4", "$23.0", "$51.1", "$1.2"], ["$26.7", "$21.4", "$51.6", "$1.4"], ["$26.5", "$25.2", "$55.3", "$1.3"], ["$28.3", "$26.6", "$55.5", "$1.6"]]
+PROB_BANK = [4, 5, 6, 7, 8, 9]
+
+# COLOR CODE: REB, GREEN, BLUE
+COLOR_WHITE = [255, 255, 255]
+COLOR_RED = [255, 0, 0]
+COLOR_BLACK = [0, 0, 0]
+
+MONITOR_WIDTH = 50
+MONITOR_DIST = 55
+MONITOR_SIZE = [1280,800]
+MONITOR_UNITS = 'pix'
+WINDOW_SIZE = (1200,900)
+
+# STIMULUS POSITIONS
+PARTNER_TEXT = (-150,300)
+YOU_TEXT = (150,300)
+PARTNER_CIRCLE = (-150,150)
+YOU_CIRCLE = (150,150)
+GAMBLE_L_CIRCLE = (-150,-150) 
+GAMBLE_R_CIRCLE = (150,-150)
+LINE_POS = [(-300,0), (300,0)]
+REVEALPOS_POS = (0, -150)
+
+NEWROUND_SIZE = 100
+TEXT_SIZE = 40
+CIRCLE_SIZE = 250
+
+ROUND_COUNT = 96
+
+
+#===================================================================================================
+# TASK CLASSES
+#===================================================================================================
+
+class LocalGame:
+
+	# image banks
+	imgBank_inactive = []
+	imgBank_cue = []
+	imgBank_active = []
+	imgBank_control = []
+
+	# variables
+	round = None
+	subID = None
+	control_bank = None
+	localGraphics = None
+	state = None
+	crtIndex = None
+	crtChoice = None
+	saveFrame = None
+
+	def __init__(self):
+		self.round = 0
+		self.subID = input('Please enter 4-digit subject identifier (i.e., 0999): ')
+		while os.path.exists('data/PREEMPT1_%s-PeerInfluenceData.txt' % self.subID):
+			self.subID = input('Data already exists for subject identifier %s. Please provide another ID: ' % self.subID)
+		self.control_bank = None
+		self.localGraphics = GameGraphics()
+		self.state = Interact()
+		self.crtIndex = None
+		self.crtChoice = None
+		self.saveFrame = []
+		self.start()
+
+	def start(self):
+		self.generateControlBank()
+		shuffle(self.control_bank)
+		self.generateImgBanks()
+
+	def generateControlBank(self):
+		chosenvectors = list(range(8))
+		shuffle(chosenvectors)
+		chosenvectors = chosenvectors[:4]
+		chosenprobs = list(range(4,10))
+		chosenconds = list(range(0,3)) # 0 is solo, 1 is info safe, 2 is info risky
+		self.control_bank = []
+		for x in chosenvectors:
+			for y in chosenprobs:
+				for z in chosenconds:
+					self.control_bank.append({ 
+						"bank" : x,
+						"safe_Hpayoff" : PAYOFF_BANK[x][0],
+						"safe_Lpayoff" : PAYOFF_BANK[x][1],
+						"risky_Hpayoff" : PAYOFF_BANK[x][2],
+						"risky_Lpayoff" : PAYOFF_BANK[x][3],
+						"p_gamble" : y/10,
+						"condition" : z
+						})
+
+	def generateImgBanks(self):
+		option_order = ["A", "B"]
+		vector_order = ["V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7"]
+		prob_order = ["19", "28", "37", "46", "55", "64", "73", "82", "91", "10"]
+		# define imgBank_active
+		for i in option_order:            
+			tmp4active2 = []
+			for j in vector_order:
+				tmp4active1 = []
+				for k in prob_order:
+					tmp4active1.append("ACTIVE_P" + k + "_" + j + "_" + i + ".jpg")
+				tmp4active2.append(tmp4active1)
+			self.imgBank_active.append(tmp4active2)
+
+		# define imgBank_inactive & imgBank_cue	
+		for i in prob_order:
+			self.imgBank_inactive.append("INACTIVE_P" + i + ".jpg")
+			self.imgBank_cue.append("CUE_P" + i + ".jpg")
+			self.imgBank_control.append("CONTROL_P" + i + ".jpg")	
+
+	def revealPosition(self):
+		self.localGraphics.positionScreenTxt(["Position 1", "Position 2"], 0)
+		self.localGraphics.positionScreenImg(self.imgBank_inactive[9])
+		sleep(6)
+		self.localGraphics.positionScreenTxt(["Partner", "You"], 1)
+		sleep(4)
+		return
+
+	def runStage0(self):
+		self.localGraphics.clearDisplay()
+		self.localGraphics.newroundScreen()
+		self.crtIndex = self.control_bank[self.round]
+		sleep(uniform(2, 3))
+		return
+
+	def runSingleStage1(self):
+		self.localGraphics.clearDisplay()
+		partner = self.imgBank_control[int(self.crtIndex['p_gamble']*10-1)]
+		target = self.imgBank_inactive[int(self.crtIndex['p_gamble']*10-1)]
+		choiceL = self.imgBank_active[0][self.crtIndex['bank']][int(self.crtIndex["p_gamble"]*10-1)]
+		choiceR = self.imgBank_active[1][self.crtIndex['bank']][int(self.crtIndex["p_gamble"]*10-1)]
+		imglist = [partner, target, choiceL, choiceR]
+		self.localGraphics.baseScreen(imglist, 0)
+		sleep(6)
+		return
+
+	def runSingleStage2(self):
+		cue = self.imgBank_cue[int(self.crtIndex['p_gamble']*10-1)]
+		self.localGraphics.cueScreen(cue)
+		self.crtChoice = self.state.selectChoice()
+		return
+
+	def runSingleStage3(self):
+		if self.crtChoice == "L":
+			decision = self.imgBank_active[0][self.crtIndex['bank']][int(self.crtIndex['p_gamble']*10-1)]
+			self.crtIndex['choice'] = 0
+		elif self.crtChoice == "R":
+			decision = self.imgBank_active[1][self.crtIndex['bank']][int(self.crtIndex['p_gamble']*10-1)]
+			self.crtIndex['choice'] = 1
+		self.localGraphics.reviewScreen(decision)
+		sleep(2)
+		return
+
+	def runGroupStage1(self):
+		self.localGraphics.clearDisplay()
+		partner = self.imgBank_cue[int(self.crtIndex['p_gamble']*10-1)]
+		target = self.imgBank_inactive[int(self.crtIndex['p_gamble']*10-1)]
+		choiceL = self.imgBank_active[0][self.crtIndex['bank']][int(self.crtIndex["p_gamble"]*10-1)]
+		choiceR = self.imgBank_active[1][self.crtIndex['bank']][int(self.crtIndex["p_gamble"]*10-1)]
+		imglist = [partner, target, choiceL, choiceR]
+		self.localGraphics.baseScreen(imglist, 1)
+		sleep(uniform(1.8, 3.8))
+		return
+
+	def runGroupStage2(self):
+		if self.crtIndex['condition'] == 1: # Partner chose safe (left)
+			choiceP = self.imgBank_active[0][self.crtIndex['bank']][int(self.crtIndex["p_gamble"]*10-1)]
+		elif self.crtIndex['condition'] == 2: # Partner chose risky (right)
+			choiceP = self.imgBank_active[1][self.crtIndex['bank']][int(self.crtIndex["p_gamble"]*10-1)]
+		self.localGraphics.partnerreviewScreen(choiceP)
+		sleep(2)
+		return
+
+	def runGroupStage3(self):
+		cue = self.imgBank_cue[int(self.crtIndex['p_gamble']*10-1)]
+		self.localGraphics.cueScreen(cue)
+		self.crtChoice = self.state.selectChoice()
+		return
+
+	def runGroupStage4(self):
+		if self.crtChoice == "L":
+			decision = self.imgBank_active[0][self.crtIndex['bank']][int(self.crtIndex['p_gamble']*10-1)]
+			self.crtIndex['choice'] = 0
+		elif self.crtChoice == "R":
+			decision = self.imgBank_active[1][self.crtIndex['bank']][int(self.crtIndex['p_gamble']*10-1)]
+			self.crtIndex['choice'] = 1
+		self.localGraphics.reviewScreen(decision)
+		sleep(2)
+		return
+
+	def saveData(self):
+		saveFrame = []
+		titleFrame = []
+		self.crtIndex['trial'] = self.round + 1
+		self.crtIndex['subID'] = self.subID
+		for x in list(self.crtIndex.keys()):
+			saveFrame.append(self.crtIndex[x])
+			if self.round == 0:
+				titleFrame.append(x)
+		for i, x in enumerate(saveFrame):
+			saveFrame[i] = str(x)
+		with open('data/PREEMPT1_%s-PeerInfluenceData.txt' % self.subID, 'a') as file:
+			if self.round == 0:
+				file.write('\t'.join(titleFrame[0:]) + '\n')
+			file.write('\t'.join(saveFrame[0:]) + '\n')
+
+class GameGraphics:
+
+	monitor = None
+	win = None
+	imgRoot = None
+	staticStim = None
+
+	def __init__(self):
+		self.monitor = monitors.Monitor('expMonitor', width = MONITOR_WIDTH, distance = MONITOR_DIST)
+		self.monitor.setSizePix(MONITOR_SIZE)
+		self.monitor.saveMon()
+		self.win = Window(size = WINDOW_SIZE, fullscr=False, screen=0, allowGUI=False, allowStencil=False, monitor='expMonitor', color=COLOR_BLACK, colorSpace='rgb255', blendMode='avg', useFBO=True, units=MONITOR_UNITS)
+		self.imgRoot = "ImgSource/"
+		self.staticStim = []
+
+	def newroundScreen(self):
+		text = TextStim(self.win, text="New Round", pos=(0, 0), height=NEWROUND_SIZE, color=COLOR_WHITE, colorSpace='rgb255')
+		text.draw()
+		if len(self.staticStim) > 0:
+			for x in range(len(self.staticStim)):
+				for y in list(self.staticStim[x].keys()):
+					self.staticStim[x][y].setAutoDraw(False)
+			self.staticStim = []
+		self.win.update()
+
+	def positionScreenImg(self, img):
+		img_pStim = {
+		"img_p1" : ImageStim(self.win, image="%s%s" % (self.imgRoot, img), pos=PARTNER_CIRCLE, size=CIRCLE_SIZE),
+		"img_p2" : ImageStim(self.win, image="%s%s" % (self.imgRoot, img), pos=YOU_CIRCLE, size=CIRCLE_SIZE)
+		}
+		self.staticStim.append(img_pStim)
+		for x in list(self.staticStim[0].keys()):
+			self.staticStim[0][x].setAutoDraw(True)
+		self.win.update()
+
+	def positionScreenTxt(self, titlelist, timepoint):
+		if timepoint == 0:
+			p2color = COLOR_WHITE
+		else:
+			p2color = COLOR_RED
+		txt_pStim = {
+		"txt_p1" : TextStim(self.win, text=titlelist[0], pos=PARTNER_TEXT, height=TEXT_SIZE, color=COLOR_WHITE, colorSpace='rgb255'),
+		"txt_p2" : TextStim(self.win, text=titlelist[1], pos=YOU_TEXT, height=TEXT_SIZE, color=p2color, colorSpace='rgb255'),
+		"txt_bottom" : TextStim(self.win, text="Choosing order...", pos=REVEALPOS_POS, height=TEXT_SIZE, color=COLOR_WHITE, colorSpace='rgb255')
+		}
+		if timepoint != 0:
+			txt_pStim["txt_bottom"].text = "You will be POSITION 2"
+		for x in list(txt_pStim.keys()):
+			txt_pStim[x].draw()
+		if timepoint != 0:
+			self.win.update()
+
+	def baseScreen(self, imglist, isGroup):
+		bStim = {
+		"txt_partner" : TextStim(self.win, text="Partner", pos=PARTNER_TEXT, height=TEXT_SIZE, color=COLOR_WHITE, colorSpace='rgb255'),
+		"txt_you" : TextStim(self.win, text="You", pos=YOU_TEXT, height=TEXT_SIZE, color=COLOR_WHITE, colorSpace='rgb255'),
+		"img_partner" : ImageStim(self.win, image="%s%s" % (self.imgRoot, imglist[0]), pos=PARTNER_CIRCLE, size=CIRCLE_SIZE),
+		"img_you" : ImageStim(self.win, image="%s%s" % (self.imgRoot, imglist[1]), pos=YOU_CIRCLE, size=CIRCLE_SIZE),
+		"line" : Line(self.win, start=LINE_POS[0], end=LINE_POS[1], lineColor=COLOR_WHITE),
+		"img_choiceL" : ImageStim(self.win, image="%s%s" % (self.imgRoot, imglist[2]), pos=GAMBLE_L_CIRCLE, size=CIRCLE_SIZE),
+		"img_choiceR" : ImageStim(self.win, image="%s%s" % (self.imgRoot, imglist[3]), pos=GAMBLE_R_CIRCLE, size=CIRCLE_SIZE)
+		}
+		if isGroup == 0:
+			bStim["txt_partner"].text = "------"
+		self.staticStim.append(bStim)
+		for x in list(self.staticStim[0].keys()):
+			self.staticStim[0][x].setAutoDraw(True)
+		self.win.update()
+		if isGroup == 0:
+			self.staticStim[0]['img_you'].setAutoDraw(False)
+		elif isGroup == 1:
+			self.staticStim[0]['img_partner'].setAutoDraw(False)
+
+	def cueScreen(self, img):
+		cStim = {
+		"img_cue" : ImageStim(self.win, image="%s%s" % (self.imgRoot, img), pos=YOU_CIRCLE, size=CIRCLE_SIZE),
+		"txt_cue" : TextStim(self.win, text="You", pos=YOU_TEXT, height=TEXT_SIZE, color=COLOR_RED, colorSpace='rgb255')
+		}
+		for x in list(cStim.keys()):
+			cStim[x].draw()
+		self.win.update()
+
+	def reviewScreen(self, img):
+		rStim = {
+		"img_choice" : ImageStim(self.win, image="%s%s" % (self.imgRoot, img), pos=YOU_CIRCLE, size=CIRCLE_SIZE),
+		"txt_you" : TextStim(self.win, text="You", pos=YOU_TEXT, height=TEXT_SIZE, color=COLOR_WHITE, colorSpace='rgb255')
+		}
+		for x in list(rStim.keys()):
+			rStim[x].draw()
+		self.win.update()
+
+	def partnerreviewScreen(self, img):
+		prStim = {
+		"img_pchoice" : ImageStim(self.win, image="%s%s" % (self.imgRoot, img), pos=PARTNER_CIRCLE, size=CIRCLE_SIZE),
+		"txt_partner" : TextStim(self.win, text="Partner", pos=PARTNER_TEXT, height=TEXT_SIZE, color=COLOR_WHITE, colorSpace='rgb255')
+		}
+		self.staticStim.append(prStim)
+		for x in list(self.staticStim[1].keys()):
+			self.staticStim[1][x].setAutoDraw(True)
+		self.win.update()
+		self.staticStim[0]['img_you'].setAutoDraw(False)
+
+	def clearDisplay(self):
+		self.win.flip()
+
+class Interact:
+
+	choice = None
+
+	def __init__(self):
+		self.choice = None
+
+	def selectChoice(self):
+		def keyPress(key):
+			try:
+				if key.char == '1':
+					print("Left Choice!")
+					self.choice = "L"
+					listener.stop()
+				elif key.char == '0':
+					print("Right Choice!")
+					self.choice = "R"
+					listener.stop()
+			except:
+				pass
+			if key == keyboard.Key.esc:
+				listener.stop()
+				core.quit()
+		with keyboard.Listener(on_press=keyPress) as listener:
+			listener.join()
+		return(self.choice)
+
+def runRound(game):
+	game.runStage0()
+	if game.crtIndex['condition'] == 0:
+		game.runSingleStage1()
+		game.runSingleStage2()
+		game.runSingleStage3()
+	else:
+		game.runGroupStage1()
+		game.runGroupStage2()
+		game.runGroupStage3()
+		game.runGroupStage4()
+	game.saveData()
+	game.round += 1
+
+if __name__ == "__main__":
+	if not os.path.exists('data/'):
+		os.mkdir('data/')
+	game = LocalGame()
+	game.revealPosition()
+	for x in range(ROUND_COUNT):
+		runRound(game)
