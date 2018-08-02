2118507604d6bc876d716e3fb0248768fb11ff6b
Kyle LaFollette <kylelafollette@dhcp-10-142-230-104.uawifi.arizona.edu>
2018-08-02 10:03:07 -0700

    First commit

--- PREEMPT Search Tool/SearchTool.py
+++ PREEMPT Search Tool/SearchTool.py
@@ -2,7 +2,6 @@
 Author: Kyle J. LaFollette
 Department of Psychiatry, University of Arizona
 Correspondance: kjlafoll@psychiatry.arizona.edu
-
 This script searches the PREEMPT shared drive for files matching keyword
 specifications. It begins by allowing you to specify the subdirectories 
 within PREEMPT that you'd like to narrow your search to, which speeds up
@@ -11,7 +10,6 @@ are specified in groups, such that all keywords in a group need to be found
 in a filename to match. Multiple groups can be specified, and only one group
 need be satisfied to match. Lastly, you specify a file extension to include
 in your search.
-
 Currently, matched files are copied to a folder written to the local user's
 desktop titled SearchTool_Matches.
 """
@@ -64,7 +62,7 @@ class SearchTool:
 					self.connected = True
 					break
 			if self.connected == False:
-				check = input("Not connected to Killgore_Scan Shared Drive. Please connect and press any key to restart.")
+				check = input("\nNot connected to Killgore_Scan Shared Drive. Please connect and press any key to restart.")
 
 	def specifier(self):
 
@@ -75,23 +73,23 @@ class SearchTool:
 
 		while exkeys == True:
 			if crtkey == 0:
-				key.append([input("Enter a keyword to search for. Or enter NONE to collect all files in the search directory: ")])
+				key.append([input("\nEnter a keyword to search for. Or enter NONE to collect all files in the search directory: ")])
 			else:
-				key.append([input("Enter another keyword to search for IN SEPARATE FILENAMES. This is useful if you have multiple file types that you are searching for. Or enter NONE to continue: ")])
+				key.append([input("\nEnter another keyword to search for IN SEPARATE FILENAMES. This is useful if you have multiple file types that you are searching for. Or enter NONE to continue: ")])
 			if key[crtkey][0] != "NONE":
 				while "NEXT" not in [x for x in key[crtkey]]:
-					print("\nCurrent keywords to search for: %s\n" % key)
-					key[crtkey].append(input("Enter another keyword to search for IN THE SAME FILENAME. Or enter NEXT to continue: "))
+					print("\nCurrent keywords to search for: %s" % key)
+					key[crtkey].append(input("\nEnter another keyword to search for IN THE SAME FILENAME. Or enter NEXT to continue: "))
 				key[crtkey] = key[crtkey][:-1]
 			else:
 				exkeys = False
 			crtkey += 1
 		key = key[:-1]
 
-		ext.append(".%s" % input("Enter a file extension to search for. Enter only the alphanumeric symbols after the '.' (i.e., 'txt', 'csv'). Or enter ALL to collect all files matching previous specifications: "))
+		ext.append(".%s" % input("\nEnter a file extension to search for. Enter only the alphanumeric symbols after the '.' (i.e., 'txt', 'csv'). Or enter ALL to collect all files matching previous specifications: "))
 		if ext[0] != ".ALL":
 			while "NEXT" not in ext:
-				ext.append(input("Enter another extension to search for. Collected files will contain EITHER extension. Or enter NEXT to continue: "))
+				ext.append(input("\nEnter another extension to search for. Collected files will contain EITHER extension. Or enter NEXT to continue: "))
 		ext = ext[:-1]
 
 		self.matchlist = []
@@ -114,7 +112,7 @@ class SearchTool:
 			count += 1
 			if count >= 100:
 				cmtp += 1
-				print("%s files checked for match..." % (100*cmtp))
+				print("\n%s files checked for match..." % (100*cmtp))
 				count = 0
 
 	def director(self):
@@ -122,7 +120,7 @@ class SearchTool:
 		self.newroot = None
 
 		while self.newroot != "ALL":
-			print("Search tool directed to %s" % self.root)
+			print("\nSearch tool directed to %s" % self.root)
 			subdirs = []
 			actualdirs = []
 			for s in os.listdir(self.root):
@@ -130,16 +128,16 @@ class SearchTool:
 					actualdirs.append(s)
 			if len(actualdirs) == 0:
 				break
-			print("Subdirectories listed below:")
+			print("\nSubdirectories listed below:")
 			for i, x in enumerate(actualdirs):
 				print("%s. %s" % (i+1, x))
 				subdirs.append([i+1, x])
 			subdirs.append(["ALL", None])
 
-			self.newroot = input("Enter the number for the subdirectory you'd like to search. Or enter ALL to search all subdirectories: ")
+			self.newroot = input("\nEnter the number for the subdirectory you'd like to search. Or enter ALL to search all subdirectories: ")
 			while self.newroot not in [str(x[0]) for x in subdirs]:
-				print("Not a valid input.")
-				self.newroot = input("Enter the number for the subdirectory you'd like to search. Or enter ALL to search all subdirectories: ")
+				print("\nNot a valid input.")
+				self.newroot = input("\nEnter the number for the subdirectory you'd like to search. Or enter ALL to search all subdirectories: ")
 			if self.newroot in [str(x[0]) for x in subdirs][:-1]:
 				self.root = os.path.join(self.root, subdirs[int(self.newroot)-1][1])
 
@@ -152,7 +150,7 @@ class SearchTool:
 				count += 1
 				if count >= 100:
 					cmtp += 1
-					print("%s files collected..." % (100*cmtp))
+					print("\n%s files collected..." % (100*cmtp))
 					count = 0
 				self.pathslist.append(os.path.join(p, n))
 				self.fileslist.append(n)
@@ -160,29 +158,29 @@ class SearchTool:
 	def writer(self):
 
 		if len(self.matchlist) > 0:
-			copycheck = input("%s file(s) found matching specifications. Would you like to copy files? (y/n): " % len(self.matchlist))
+			copycheck = input("\n%s file(s) found matching specifications. Would you like to copy files? (y/n): " % len(self.matchlist))
 			while copycheck not in [x for x in ['y', 'n']]:
-				copycheck = input("Not a valid input. Would you like to copy files? (y/n): ")
+				copycheck = input("\nNot a valid input. Would you like to copy files? (y/n): ")
 			if copycheck == "y":
-				print("Current saving directory: %s" % self.saveDir)
-				doublecheck = input("Would you like to save here? (y/n): ")
+				print("\nCurrent saving directory: %s" % self.saveDir)
+				doublecheck = input("\nWould you like to save here? (y/n): ")
 				while doublecheck not in [x for x in ["y", "n"]]:
-					doublecheck = input("Not a valid input. Would you like to save to %s? (y/n): " % self.saveDir)
+					doublecheck = input("\nNot a valid input. Would you like to save to %s? (y/n): " % self.saveDir)
 				if doublecheck == "y":
-					newFolder = input("Enter the name of a new folder to save to (will be created in %s): " % self.saveDir)
+					newFolder = input("\nEnter the name of a new folder to save to (will be created in %s): " % self.saveDir)
 					if not os.path.isdir(self.saveDir):
 						os.mkdir(self.saveDir)
 					if not os.path.isdir(self.saveDir+newFolder+self.tag):
 						os.mkdir(self.saveDir+newFolder+self.tag)
 					for x in self.matchlist:
 						copyfile(self.pathslist[x[0]], "%s%s" % (self.saveDir+newFolder+self.tag, os.path.basename(self.pathslist[x[0]])))
-					self.finished = input("Files saved. Task complete. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ")
+					self.finished = input("\nFiles saved. Task complete. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ")
 				else:
-					self.finished = input("Change the saving directory in the source code if you'd like to save elsewhere. Task Complete. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ")
+					self.finished = input("\nChange the saving directory in the source code if you'd like to save elsewhere. Task Complete. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ")
 			else:
-				self.finished = input("Good bye. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ")
+				self.finished = input("\nGood bye. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ")
 		else:
-			self.finished = input("0 files found matching specifications. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ") 
+			self.finished = input("\n0 files found matching specifications. Enter 'r' to restart program, 's' to respecify keywords, 'd' to redirect the search tool, and any other key to exit: ") 
 
 if __name__ == "__main__":
 
@@ -204,8 +202,3 @@ if __name__ == "__main__":
 			executor.specifier()
 		else:
 			executor.cont = False
-
-
-
-
-
